99af7e9bc1788336139147a08cbab7b51881f592
AbdullahZe <abdullahmaaz2005@gmail.com>
2024-12-16 11:22:48 +0530

    backend2

--- login-page/public/backend/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///upskill.db'  # Replace with your database
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your-jwt-secret-key'
